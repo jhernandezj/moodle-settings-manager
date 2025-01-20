813e31ff5dd8e1fad6928620866c802a3c4e782c
Javier Hernandez <jhernandezj@gmail.com>
2025-01-20 14:11:46 +0100

    Implement basic settings functionality

--- pkg/settings/manager.go
+++ pkg/settings/manager.go
@@ -1,25 +1,36 @@
 package settings
 
+import "fmt"
+
 // Manager handles Moodle settings operations
 type Manager struct {
-	config interface{}
+	config   interface{}
+	settings map[string]string
 }
 
 // New creates a new settings manager instance
 func New(config interface{}) *Manager {
 	return &Manager{
 		config: config,
+		settings: map[string]string{
+			"site_name":        "My Moodle Site",
+			"maintenance_mode": "disabled",
+			"theme":            "boost",
+			"lang":             "en",
+		},
 	}
 }
 
 // GetSetting retrieves a setting value by name
 func (m *Manager) GetSetting(name string) (string, error) {
-	// TODO: Implement setting retrieval
-	return "Test", nil
+	if value, exists := m.settings[name]; exists {
+		return value, nil
+	}
+	return "", fmt.Errorf("setting %s not found", name)
 }
 
 // UpdateSetting updates a setting value
 func (m *Manager) UpdateSetting(name, value string) error {
-	// TODO: Implement setting update
+	m.settings[name] = value
 	return nil
 }
